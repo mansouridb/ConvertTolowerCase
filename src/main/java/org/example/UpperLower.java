189b0afecc8223532c9d2990b6fcaa3f24c73a4c
Majid <Mansouridb@gmail.com>
2021-04-21 13:54:09 +0200

    Convert the following String: "CAPS EQUALS SCREAMING" to lowercase and print it out. Then convert it back to uppercase and print it out again.

---
+++ src/main/java/org/example/UpperLower.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class UpperLower {
+    public static void main(String[] args) {
+    String S1= new String("CAPS EQUALS SCREAMING");
+    System.out.println("The lower format  of " + "CAPS EQUALS SCREAMING "+ " will be "+ S1.toLowerCase() );
+    System.out.println("Convert to original format"+ "will be "+ S1.toUpperCase()) ;
+
+    }
+}
